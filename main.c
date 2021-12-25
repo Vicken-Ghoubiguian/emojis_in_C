5b4e8d2aa0cea215ed3d792a3a9b1a72de702aa6
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-12-25 19:56:53 +0100

    Include a file... 2

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
+//
 #include <stdio.h>
 
+// Definition of the main function of this file...
 int main() {
-    printf("\U0001f984");
-    return 0;
+
+	// Display the unicorn emoji from its unicode...
+	printf("\U0001f984");
+
+	// Execution without any error...
+	return 0;
 }
