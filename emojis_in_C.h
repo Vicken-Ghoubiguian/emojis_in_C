565eaf39bf9b536270998cb65bce2ad427c73087
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-14 11:19:10 +0100

    HJ2

--- emojis_in_C.h
+++ emojis_in_C.h
@@ -24,7 +24,6 @@ char* getEmojiCategory(Emoji emoji);
 //
 char* getEmojiSubCategory(Emoji emoji);
 
-#endif
-
 // =================================== Definition of the emoji recovery functions... ===================================
 
+#endif
