dc6962ca500d7deadd5b13d72159a68e9b73eee9
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-14 11:02:49 +0100

    Tryckty

--- emojis_in_C.h
+++ emojis_in_C.h
@@ -1,6 +1,27 @@
 #ifndef EMOJIS_IN_C
 #define EMOJIS_IN_C
 
+// =================================== Definition of the 'Emoji' struct... ===================================
 
+//
+typedef struct Emoji Emoji;
+
+//
+Emoji emojiInitializer(int currentNumber, char* currentCode, char* currentName, char* currentCategory, char* currentSubCategory);
+
+//
+int getEmojiNumber(Emoji emoji);
+
+//
+char* getEmojiCode(Emoji emoji);
+
+//
+char* getEmojiName(Emoji emoji);
+
+//
+char* getEmojiCategory(Emoji emoji);
+
+//
+char* getEmojiSubCategory(Emoji emoji);
 
 #endif
