94a101e6a1fb63317c08ec7f0ff87844ebd1e6ab
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-14 09:15:01 +0100

    Definition of type 'Emoji'...

--- emoji.c
+++ emoji.c
@@ -1,2 +1,17 @@
 //
 #include <stdio.h>
+
+//
+typedef struct {
+
+    int number;
+    char* code;
+    char* name;
+    char* category
+    char* subCategory;
+
+} Emoji;
+
+//
+typedef Emoji Emoji;
+
