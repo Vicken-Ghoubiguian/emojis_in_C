a61e3f184e318f056c0e2108922ae2002b574e47
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-17 09:50:03 +0100

    PoSPS...

--- main.c
+++ main.c
@@ -8,22 +8,25 @@ int main() {
 	//
 	Emoji *unicornEmoji;
 	Emoji *catEmoji;
-	//Emoji *cryingCatEmoji;
+	Emoji *cryingCatEmoji;
 
 	//
 	unicornEmoji = emojiInitializer(550, "\U0001F984", "Hey 2", "Hey 3", "Hey 4");
 	catEmoji = emojiInitializer(541, "\U0001F431", "Hey 2", "Hey 3", "Hey 4");
+	cryingCatEmoji = emojiInitializer(122, "\U0001F63F", "Hey 2", "Hey 3", "Hey 4");
 
 	//
 	char* unicornEmojiCode = getEmojiCode(unicornEmoji);
 	char* catEmojiCode = getEmojiCode(catEmoji);
+	char* cryingCatEmojiCode = getEmojiCode(cryingCatEmoji);
 
 	//
 	printf("%s", unicornEmojiCode);
 	printf("%s", catEmojiCode);
+	printf("%s", cryingCatEmojiCode);
 
 	// Display the crying cat emoji from its unicode...
-	printf("\U0001F63F");
+	//printf("\U0001F63F");
 
 	// Execution without any error...
 	return 0;
