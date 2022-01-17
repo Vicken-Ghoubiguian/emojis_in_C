22a3ebed7c6dfa144e89a7bf9d103bc23d4c163c
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-17 09:50:22 +0100

    DJ...

--- main.c
+++ main.c
@@ -25,9 +25,6 @@ int main() {
 	printf("%s", catEmojiCode);
 	printf("%s", cryingCatEmojiCode);
 
-	// Display the crying cat emoji from its unicode...
-	//printf("\U0001F63F");
-
 	// Execution without any error...
 	return 0;
 }
